96b28b596a631cfa3b3052917fe9689d2e4a5757
Tanja <tanja120a@gmail.com>
2021-12-16 08:08:02 +0100

    Redirect url

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,8 @@ module.exports = function (passport) {
     passport.use( new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback'
+        callbackURL: 'http://peaceful-everglades-52135.herokuapp.com/auth/google/callback',
+       // callbackURL: '/auth/google/callback'
     },
     async (accessToken, refreshToken, profile, done) => {
         //console.log(profile);
